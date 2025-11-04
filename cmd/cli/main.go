a429686c05167ba81de4820fc9c476c3a3927f80
JanMalch <25508038+JanMalch@users.noreply.github.com>
2025-11-04 22:44:52 +0100

    feat: use latest project version for help

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,17 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/alecthomas/kong"
 	"github.com/janmalch/roar/internal/run"
+	"github.com/janmalch/roar/internal/steps"
 	"github.com/janmalch/roar/models"
+	"github.com/janmalch/roar/pkg/git"
 	"github.com/janmalch/roar/util"
 )
 
 var VERSION = "0.16.0"
 
 func main() {
+	// Using current repo version for help text, if people copy the commands
+	r := git.NewRepo("")
+	tag, _, _ := steps.DetermineLatest(r)
+	if tag == "" {
+		tag = "v1.0.0"
+	}
+
 	var cli models.CLI
 	kong.Parse(&cli,
 		kong.Name("roar"),
@@ -23,9 +33,9 @@ func main() {
 			"\n\t3. parse them as conventional commits"+
 			"\n\t4. determine the next version"+
 			"\n\t5. create a tagged commit\n"+
-			"Thus, roar doesn't lock you into anything.\nFor example amending something to the release commit of a v1.0.0 is as simple as\n\t"+
-			util.Bold("git tag -d v1.0.0\n\tgit add .\n\tgit commit --amend --no-edit\n\tgit tag -a \"v1.0.0\" -m \"Release v1.0.0\"")+"\n"+
-			"To undo all changes, you only have to run\n\t"+util.Bold("git tag -d v1.0.0\n\tgit reset --hard HEAD^")+"\n"+
+			fmt.Sprintf("Thus, roar doesn't lock you into anything.\nFor example amending something to the release commit of a %s is as simple as\n\t", tag)+
+			util.Bold(fmt.Sprintf("git tag -d %s\n\tgit add .\n\tgit commit --amend --no-edit\n\tgit tag -a \"%s\" -m \"Release %s\"", tag, tag, tag))+"\n"+
+			"To undo all changes, you only have to run\n\t"+util.Bold(fmt.Sprintf("git tag -d %s\n\tgit reset --hard HEAD^", tag))+"\n"+
 			"roar will "+util.Bold("never")+" push automatically.",
 		),
 		kong.Vars{
